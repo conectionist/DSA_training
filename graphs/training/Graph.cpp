4d76bd44d26761225729c549b9a6c76f2f35c36a
conectionist <dan.manson@protonmail.com>
2026-06-18 15:13:56 +0300

    turned training methods into stubs

--- graphs/training/Graph.cpp
+++ graphs/training/Graph.cpp
@@ -1,11 +1,5 @@
 #include "Graph.h"
 
-#include <queue>
-#include <stack>
-
-using std::queue;
-using std::stack;
-
 Graph::Graph(const vector<vector<int>>& adjacencyList)
 {
     m_adjacencyList = adjacencyList;
@@ -14,270 +8,96 @@ Graph::Graph(const vector<vector<int>>& adjacencyList)
 
 void Graph::bfs(function<void(int)> process)
 {
-    if(m_nodeCount == 0)
-        return;
-
-    vector<bool> visited(m_nodeCount, false);
-
-    for(int i = 0 ; i < m_nodeCount ; i++)
-        if(!visited[i])
-            bfsInternal(visited, i, process);
+    (void)process;
 }
 
 void Graph::dfsIterative(function<void(int)> process)
 {
-    if(m_nodeCount == 0)
-        return;
-
-    vector<bool> visited(m_nodeCount, false);
-
-    for(int i = 0 ; i < m_nodeCount ; i++)
-        if(!visited[i])
-            dfsIterativeInternal(visited, i, process);
+    (void)process;
 }
 
 void Graph::dfsRecursive(function<void(int)> process)
 {
-    if(m_nodeCount == 0)
-        return;
-
-    vector<bool> visited(m_nodeCount, false);
-
-    dfsRecursiveInternal(visited, 0, process);
+    (void)process;
 }
 
 bool Graph::isCyclicIterative()
 {
-    auto topoSortedNodes = getTopologicalOrder();
-
-    return topoSortedNodes.size() != m_nodeCount;
+    return false;
 }
 
 bool Graph::isCyclicRecursive()
 {
-    vector<bool> visited(m_nodeCount, false);
-    vector<bool> recStack(m_nodeCount, false);
-
-    for(int i = 0 ; i < m_nodeCount ; i++)
-        if(isCyclicRecursiveHelper(i, visited, recStack))
-            return true;
-
     return false;
 }
 
 vector<int> Graph::getTopologicalOrder()
 {
-    vector<int> inDegrees(m_nodeCount, 0);
-    vector<int> topoSortedNodes;
-    queue<int> q;
-
-    for(int i = 0 ; i < m_nodeCount ; i++)
-        for(auto neigh : m_adjacencyList[i])
-            inDegrees[neigh]++;
-    
-    for(int i = 0 ; i < m_nodeCount ; i++)
-        if(inDegrees[i] == 0)
-            q.push(i);
-    
-    while(!q.empty())
-    {
-        int current = q.front();
-        q.pop();
-
-        topoSortedNodes.push_back(current);
-
-        for(auto n : m_adjacencyList[current])
-        {
-            inDegrees[n]--;
-
-            if(inDegrees[n] == 0)
-                q.push(n);
-        }
-    }
-
-    return topoSortedNodes;
+    return {};
 }
 
 int Graph::shortestDistance(int a, int b)
 {
-    queue<int> q;
-    vector<bool> visited(m_nodeCount, false);
-    vector<int> distances(m_nodeCount, -1);
-
-    int count = 0;
-
-    q.push(a);
-    visited[a] = true;
-    distances[a] = 0;
-
-    while(!q.empty())
-    {
-        auto current = q.front();
-        q.pop();
-
-        if(current == b)
-            return distances[current];
-        
-        for(int neighbor : m_adjacencyList[current])
-            if(!visited[neighbor])
-            {
-                visited[current] = true;
-                distances[neighbor] = distances[current] + 1;
-                q.push(neighbor);
-            }
-    }
-
+    (void)a;
+    (void)b;
     return -1;
 }
 
 int Graph::componentsCount()
 {
-    if(m_nodeCount == 0)
-        return 0;
-
-    vector<bool> visited(m_nodeCount, false);
-    int count = 0;
-
-    for(int i = 0 ; i < m_nodeCount ; i++)
-        if(!visited[i])
-        {
-            bfsInternal(visited, i);
-            count++;
-        }
-
-    return count;
+    return 0;
 }
 
 vector<vector<int>> Graph::getSCCv1()
 {
-    stack<int> st;
-    vector<bool> visited(m_nodeCount, false);
-
-    for(int i = 0 ; i < m_nodeCount ; i++)
-        if(!visited[i])
-            dfsKosaraju1(i, visited, st);
-    
-    vector<vector<int>> reverseAdj(m_nodeCount);
-
-    for(int i = 0 ; i < m_nodeCount ; i++)
-        for(const auto neighbor : m_adjacencyList[i])
-            reverseAdj[neighbor].push_back(i);
-        
-    for(int i = 0 ; i < m_nodeCount ; i++)
-        visited[i] = false;
-
-    vector<vector<int>> sccs;
-    
-    while(!st.empty())
-    {
-        auto current = st.top();
-        st.pop();
-
-        if(!visited[current])
-        {
-            vector<int> scc;
-            dfsKosaraju2(current, reverseAdj, visited, scc);
-            sccs.push_back(scc);
-        }
-    }
-
-    return sccs;
+    return {};
 }
 
-bool Graph::isCyclicRecursiveHelper(int node, vector<bool> &visited, vector<bool> &recStack)
+bool Graph::isCyclicRecursiveHelper(int node, vector<bool>& visited, vector<bool>& recStack)
 {
-    if(!visited[node])
-    {
-        visited[node] = true;
-        recStack[node] = true;
-
-        for(int neighbor : m_adjacencyList[node])
-        {
-            if(!visited[neighbor] && isCyclicRecursiveHelper(neighbor, visited, recStack))
-                return true;
-            else if(recStack[neighbor])
-                return true;
-        }
-    }
-
-    recStack[node] = false;
-
+    (void)node;
+    (void)visited;
+    (void)recStack;
     return false;
 }
 
-void Graph::bfsInternal(vector<bool> &visited, int node, function<void(int)> process)
+void Graph::bfsInternal(vector<bool>& visited, int node, function<void(int)> process)
 {
-    queue<int> q;
-    q.push(node);
-    visited[node] = true;
-
-    while(!q.empty())
-    {
-        auto current = q.front();
-        q.pop();
-        process(current);
-        
-        for(int neighbor : m_adjacencyList[current])
-            if(!visited[neighbor])
-            {
-                q.push(neighbor);
-                visited[neighbor] = true;
-            }
-    }
+    (void)visited;
+    (void)node;
+    (void)process;
 }
 
-void Graph::dfsIterativeInternal(vector<bool> &visited, int node, function<void(int)> process)
+void Graph::dfsIterativeInternal(vector<bool>& visited, int node, function<void(int)> process)
 {
-    stack<int> st;
-    st.push(node);
-    
-    while(!st.empty())
-    {
-        auto current = st.top();
-        st.pop();
-
-        if(visited[current])
-            continue;
-
-        process(current);
-        visited[current] = true;
-
-        for(int neighbor : m_adjacencyList[current])
-            if(!visited[neighbor])
-                st.push(neighbor);
-    }
+    (void)visited;
+    (void)node;
+    (void)process;
 }
 
-void Graph::dfsRecursiveInternal(vector<bool> &visited, int node, function<void(int)> process)
+void Graph::dfsRecursiveInternal(vector<bool>& visited, int node, function<void(int)> process)
 {
-    process(node);
-    visited[node] = true;
-
-    for(auto neighbor : m_adjacencyList[node])
-        if(!visited[neighbor])
-            dfsRecursiveInternal(visited, neighbor, process);
+    (void)visited;
+    (void)node;
+    (void)process;
 }
 
-void Graph::dfsKosaraju1(int node, vector<bool> &visited, stack<int> &st)
+void Graph::dfsKosaraju1(int node, vector<bool>& visited, stack<int>& st)
 {
-    visited[node] = true;
-
-    for(auto neighbor : m_adjacencyList[node])
-        if(!visited[neighbor])
-            dfsKosaraju1(neighbor, visited, st);
-
-    st.push(node);
+    (void)node;
+    (void)visited;
+    (void)st;
 }
 
-void Graph::dfsKosaraju2(int node, const vector<vector<int>>& reverseAdjancencyLists, vector<bool> &visited, vector<int> &scc)
+void Graph::dfsKosaraju2(int node, const vector<vector<int>>& reverseAdjancencyLists, vector<bool>& visited, vector<int>& scc)
 {
-    visited[node] = true;
-
-    scc.push_back(node);
-
-    for(const int neghbor : reverseAdjancencyLists[node])
-        if(!visited[neghbor])
-            dfsKosaraju2(neghbor, reverseAdjancencyLists, visited, scc);
+    (void)node;
+    (void)reverseAdjancencyLists;
+    (void)visited;
+    (void)scc;
 }
 
-void printNodeValue(int n){}
+void printNodeValue(int n)
+{
+    (void)n;
+}
--- graphs/training/WeightedGraph.cpp
+++ graphs/training/WeightedGraph.cpp
@@ -1,23 +1,16 @@
 #include "WeightedGraph.h"
 #include "UnionFind.h"
 
-#include <queue>
-#include <utility>
 #include <climits>
-#include <exception>
 #include <sstream>
-#include <algorithm>
 
-#include <iostream>
+using std::stringstream;
 
-using std::priority_queue;
-using std::pair;
-
-WeightedGraph::WeightedGraph(int nodeCount) : m_adjacencyList(nodeCount), 
+WeightedGraph::WeightedGraph(int nodeCount) : m_adjacencyList(nodeCount),
                                               m_nodeCount(nodeCount)
 {}
 
-WeightedGraph::WeightedGraph(const vector<vector<Edge>> &adjacencyList) : m_adjacencyList(adjacencyList),
+WeightedGraph::WeightedGraph(const vector<vector<Edge>>& adjacencyList) : m_adjacencyList(adjacencyList),
                                                                           m_nodeCount(adjacencyList.size())
 {}
 
@@ -33,93 +26,26 @@ void WeightedGraph::addEdge(int from, int to, int weight, bool isBidirectional)
     }
 }
 
-void WeightedGraph::addEdge(const Edge &e, bool isBidirectional)
+void WeightedGraph::addEdge(const Edge& e, bool isBidirectional)
 {
     addEdge(e.from, e.to, e.weight, isBidirectional);
 }
 
 int WeightedGraph::getEdgeCount() const
 {
-    return m_edgeList.size();
+    return static_cast<int>(m_edgeList.size());
 }
 
 vector<NodePath> WeightedGraph::dijkstra(int from)
 {
-    vector<bool> visited(m_nodeCount, false);
-    vector<NodePath> results(m_nodeCount, {INT_MAX, -1});
-
-    // pair.first == cost
-    // pair.second == to
-    priority_queue<pair<int, int>, vector<pair<int, int>>, std::greater<pair<int, int>>> minHeap;
-    
-    minHeap.push({0, from});
-    results[from] = {0, -1};
-
-    while(!minHeap.empty())
-    {
-        auto current = minHeap.top().second;
-        minHeap.pop();
-        visited[current] = true;
-
-        for(const auto [from, to, weight] : m_adjacencyList[current])
-        {
-            if(visited[to])
-                continue;
-            
-            if(results[current].cost + weight < results[to].cost)
-            {
-                results[to].cost = results[current].cost + weight;
-                results[to].prev = current;
-            }
-
-            minHeap.push({weight, to});
-        }
-    }
-
-    return results;
+    (void)from;
+    return vector<NodePath>(m_nodeCount, {INT_MAX, -1});
 }
 
 vector<NodePath> WeightedGraph::bellmanFord(int from)
 {
-    vector<NodePath> results(m_nodeCount, {INT_MAX, -1});
-
-    results[from].cost = 0;
-
-    for(int i = 1 ; i <= m_nodeCount - 1 ; i++)
-    {
-        bool noUpdates = true;
-
-        for(const auto [from, to, weight] : m_edgeList)
-        {
-            if(results[from].cost == INT_MAX)
-                continue;
-
-            if(results[to].cost > results[from].cost + weight)
-            {
-                results[to].cost = results[from].cost + weight;
-                results[to].prev = from;
-
-                noUpdates = false;
-            }
-        }
-
-        if(noUpdates)
-        {
-            //std::cout << "Shortcircuited after " << i << " cycles (of " << m_nodeCount - 1 << ")" << std::endl;
-            break;
-        }
-    }
-
-    for(const auto [from, to, weight] : m_edgeList)
-    {
-        if(results[from].cost == INT_MAX)
-            continue;
-
-        if(results[to].cost > results[from].cost + weight)
-            throw std::runtime_error("Negative cycle detected!");
-    }
-
-    return results;
+    (void)from;
+    return vector<NodePath>(m_nodeCount, {INT_MAX, -1});
 }
 
 vector<vector<int>> WeightedGraph::floydWarshall()
@@ -127,130 +53,33 @@ vector<vector<int>> WeightedGraph::floydWarshall()
     int n = m_nodeCount;
     vector<vector<int>> results(n, vector<int>(n, INT_MAX));
 
-    for(int i = 0 ; i < n ; i++)
+    for(int i = 0; i < n; ++i)
         results[i][i] = 0;
 
-    for(const auto [from, to, weight] : m_edgeList)
-        results[from][to] = weight;
-
-    for(int k = 0 ; k < n ; k++)
-        for(int i = 0 ; i < n ; i++)
-            for(int j = 0 ; j < n ; j++)
-                if(results[i][k] != INT_MAX && results[k][j] != INT_MAX)
-                    results[i][j] = std::min(results[i][j], results[i][k] + results[k][j]);
-            
     return results;
 }
 
 WeightedGraph WeightedGraph::prim()
 {
-    WeightedGraph mst(m_nodeCount);
-
-    vector<bool> visited(m_nodeCount, false);
-
-    priority_queue<Edge, vector<Edge>, std::greater<Edge>> minHeap;
-
-    int start = 0;
-
-    visited[start] = true;
-
-    for(const auto& edge : m_adjacencyList[start])
-        minHeap.push(edge);
-
-    while(!minHeap.empty() && mst.getEdgeCount() / 2  < m_nodeCount - 1)
-    {
-        auto cheapest = minHeap.top();
-        minHeap.pop();
-
-        if(visited[cheapest.to])
-            continue;
-        
-        mst.addEdge(cheapest, true);
-        visited[cheapest.to] = true;
-
-        for(const auto& edge : m_adjacencyList[cheapest.to])
-            if(!visited[edge.to])
-                minHeap.push(edge);
-    }
-
-    return mst;
+    return WeightedGraph(m_nodeCount);
 }
 
 WeightedGraph WeightedGraph::kruskal()
 {
-    WeightedGraph mst(m_nodeCount);
-    UnionFind uf(m_nodeCount);
-    int count = 0;
-
-    std::sort(m_edgeList.begin(), m_edgeList.end(), std::less<Edge>());
-
-    for(const auto& e : m_edgeList)
-    {
-        if(!uf.unite(e.to, e.from))
-            continue;
-
-        mst.addEdge(e);
-        count++;
-
-        if(count == m_nodeCount - 1)
-            break;
-    }
-
-    return mst;
+    return WeightedGraph(m_nodeCount);
 }
 
 WeightedGraph WeightedGraph::borukva()
 {
-    int n = m_nodeCount;
-    WeightedGraph mst(n);
-    UnionFind dsu(n);
-
-    int componentsCount = n;
-
-    while(componentsCount > 1)
-    {
-        vector<int> cheapest(n, -1);
-
-        for(int i = 0 ; i < m_edgeList.size() ; i++)
-        {
-            auto& edge = m_edgeList[i];
-
-            int rootX = dsu.find(edge.from);
-            int rootY = dsu.find(edge.to);
-
-            if(rootX == rootY)
-                continue;
-
-            if(cheapest[rootX] == -1 || edge.weight < m_edgeList[cheapest[rootX]].weight)
-                cheapest[rootX] = i;
-            if(cheapest[rootY] == -1 || edge.weight < m_edgeList[cheapest[rootY]].weight)
-                cheapest[rootY] = i;
-        }
-
-        for(int i = 0 ; i < n ; i++)
-        {
-            if(cheapest[i] == -1)
-                continue;
-
-            auto& edge = m_edgeList[cheapest[i]];
-            
-            if(dsu.unite(edge.from, edge.to))
-            {
-                mst.addEdge(edge);
-                componentsCount--;
-            }
-        }
-    }
-
-    return mst;
+    return WeightedGraph(m_nodeCount);
 }
 
 string WeightedGraph::toString()
 {
-    std::stringstream ss;
+    stringstream ss;
 
-    for(const auto [from, to, weight] : m_edgeList)
-        ss << "(" << from << "," << to << "," << weight << ")\n";
+    for(const auto& edge : m_edgeList)
+        ss << "(" << edge.from << "," << edge.to << "," << edge.weight << ")\n";
 
     return ss.str();
 }
